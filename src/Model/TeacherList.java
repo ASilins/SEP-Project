d61ce4ad5cd3865129ecadd830e7734ec13b2be9
Ondrej <klimekondrej01@gmail.com>
2021-12-05 11:56:56 +0100

    teacher class is done and i started working on teacherlist

---
+++ src/Model/TeacherList.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class TeacherList
+{
+  private ArrayList<Teacher> teachers;
+
+  public TeacherList()
+  {
+    teachers = new ArrayList<>();
+  }
+}
