1e566113e8a124be7c495ea3caba218d1bf5c94b
Arturs Silins <silinu.arturs@gmail.com>
2021-12-06 11:44:26 +0100

    Removed spaces
    Removed spaces in class.java and classList.java

--- src/Model/Class.java
+++ src/Model/Class.java
@@ -17,86 +17,71 @@ public class Class {
      * @param studentList the students that are part of the class
      * @param courseList the courses that are part of the class
      */
-
-    public Class(String name, ArrayList<Student> studentList, ArrayList<Course> courseList)
-    {
+    public Class(String name, ArrayList<Student> studentList, ArrayList<Course> courseList) {
         className=name;
-        for (int i =0; i< studentList.size();i++)
-        {
+
+        for (int i =0; i< studentList.size();i++) {
             Student temp = studentList.get(i);
-            if (temp.getClassName().equals(className))
-            {
+
+            if (temp.getClassName().equals(className)) {
                 studentClassList.addStudent(temp);
             }
         }
-        for (int i =0; i< courseList.size();i++)
-        {
+
+        for (int i =0; i< courseList.size();i++) {
             Course temp = courseList.get(i);
-            if (temp.getCourseName().equals(courseName))
-            {
+
+            if (temp.getCourseName().equals(courseName)) {
                 courseClassList.addCourse(temp);
             }
         }
     }
 
     /**
-     * sets the class name
+     * Sets the class name
      * @param name what the class name will be set to
      */
-    public void setName(String name)
-    {
+    public void setName(String name) {
         className=name;
-
     }
 
     /**
-     * returns the class name
+     * Returns the class name
      * @return returns the name of the class
      */
-    public String getName()
-    {
+    public String getName() {
         return className;
     }
 
     /**
-     * adds a student to the list of students inside the class
+     * Adds a student to the list of students inside the class
      * @param student a student that is going to be added to the student list
      */
-    public void addStudent(Student student)
-    {
+    public void addStudent(Student student) {
         studentClassList.addStudent(student);
-
-
-
     }
 
     /**
-     * removes a student from the list of students inside the class
+     * Removes a student from the list of students inside the class
      * @param student a student that is going to be removed from the student list
      */
-    public void removeStudent(Student student)
-    {
+    public void removeStudent(Student student) {
         studentClassList.removeStudent(student);
-
     }
 
     /**
-     * adds a course to the course list of the class
+     * Adds a course to the course list of the class
      * @param course a course that is going to be added to the course list
      */
-    public void addCourse(Course course)
-    {
+    public void addCourse(Course course) {
         courseClassList.addCourse(course);
-
     }
     /**
-     * removes a course from the list of courses inside the class
+     * Removes a course from the list of courses inside the class
      * @param course a course that is going to be removed from the course list
      */
-    public void removeCourse(Course course)
-    {
+    public void removeCourse(Course course) {
         courseClassList.removeCourse(course);
-
     }
 
     /**
@@ -104,25 +89,21 @@ public class Class {
      * @param obj the object to compare with
      * @return true if the given object is equal to this class
      */
-    public boolean equals(Object obj)
-    {
-        if (!(obj instanceof Class))
-        {return false;
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Class)) {
+            return false;
         }
-        Class other = (Class)obj;
-        return className.equals(other.className)&&courseClassList.equals(other.courseClassList)&&studentClassList.equals(other.studentClassList);
 
+        Class other = (Class)obj;
+
+        return className.equals(other.className)&&courseClassList.equals(other.courseClassList)&&studentClassList.equals(other.studentClassList);
     }
 
     /**
      * returns a string representation of the class.
      * @return a string representation of the class in format. "className, courseClassList, studentClassList"
      */
-    public String toString()
-    {
+    public String toString() {
         return className + courseClassList.toString() + studentClassList.toString();
     }
-
-
-
 }
--- src/Model/ClassList.java
+++ src/Model/ClassList.java
@@ -1,7 +1,6 @@
 package Model;
 import java.util.ArrayList;
 
-
 /**
  * A class containing  list of class objects
  * @author Sid
@@ -13,43 +12,37 @@ public class ClassList {
     /**
      * No-argument constructor initializing class list.
      */
-
-    public ClassList()
-    {
+    public ClassList() {
         classes = new ArrayList<Class>();
-
     }
 
     /**
-     * adds a class to the class list.
+     * Adds a class to the class list.
      * @param classObject a class that needs to be added to the class list.
      */
-    public void addClass(Class classObject)
-    {
+    public void addClass(Class classObject) {
         classes.add(classObject);
     }
 
     /**
-     * removes a class from the class list
+     * Removes a class from the class list
      * @param classObject a class that needs to be removed from the class list
      */
-    public void removeClass(Class classObject)
-    {
+    public void removeClass(Class classObject) {
         classes.remove(classObject);
-
     }
 
     /**
-     * a method that returns a string representation of the class list.
+     * A method that returns a string representation of the class list.
      * @return a string representation of the class list in format: "Class"
      */
-    public String toString()
-    {
+    public String toString() {
         String returnString ="";
-        for (int i=0; i<classes.size(); i++)
-        {
+
+        for (int i=0; i<classes.size(); i++) {
             returnString += classes.get(i);
         }
+        
         return returnString;
     }
 
