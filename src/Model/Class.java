df8a3f2237668397875209066dd69b7f641cff88
Sid <gr.siddartha@googlemail.com>
2021-12-06 11:38:46 +0100

    Multiple file edits
    added class and classlist classes

--- src/Model/Class.java
+++ src/Model/Class.java
@@ -1,5 +1,128 @@
 package Model;
+import java.util.ArrayList;
 
+/**
+ * A class containing class information.
+ * @author Sid
+ * @version 1.0
+ */
 public class Class {
+    private String className;
+    private StudentList studentClassList;
+    private CourseList courseClassList;
+
+    /**
+     * 3 argument constructor initializing class
+     * @param name the name of the class
+     * @param studentList the students that are part of the class
+     * @param courseList the courses that are part of the class
+     */
+
+    public Class(String name, ArrayList<Student> studentList, ArrayList<Course> courseList)
+    {
+        className=name;
+        for (int i =0; i< studentList.size();i++)
+        {
+            Student temp = studentList.get(i);
+            if (temp.getClassName().equals(className))
+            {
+                studentClassList.addStudent(temp);
+            }
+        }
+        for (int i =0; i< courseList.size();i++)
+        {
+            Course temp = courseList.get(i);
+            if (temp.getCourseName().equals(courseName))
+            {
+                courseClassList.addCourse(temp);
+            }
+        }
+    }
+
+    /**
+     * sets the class name
+     * @param name what the class name will be set to
+     */
+    public void setName(String name)
+    {
+        className=name;
+
+    }
+
+    /**
+     * returns the class name
+     * @return returns the name of the class
+     */
+    public String getName()
+    {
+        return className;
+    }
+
+    /**
+     * adds a student to the list of students inside the class
+     * @param student a student that is going to be added to the student list
+     */
+    public void addStudent(Student student)
+    {
+        studentClassList.addStudent(student);
+
+
+
+    }
+
+    /**
+     * removes a student from the list of students inside the class
+     * @param student a student that is going to be removed from the student list
+     */
+    public void removeStudent(Student student)
+    {
+        studentClassList.removeStudent(student);
+
+    }
+
+    /**
+     * adds a course to the course list of the class
+     * @param course a course that is going to be added to the course list
+     */
+    public void addCourse(Course course)
+    {
+        courseClassList.addCourse(course);
+
+    }
+    /**
+     * removes a course from the list of courses inside the class
+     * @param course a course that is going to be removed from the course list
+     */
+    public void removeCourse(Course course)
+    {
+        courseClassList.removeCourse(course);
+
+    }
+
+    /**
+     * Compares the name, courseClassList and studentClassList of the class of two classes
+     * @param obj the object to compare with
+     * @return true if the given object is equal to this class
+     */
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Class))
+        {return false;
+        }
+        Class other = (Class)obj;
+        return className.equals(other.className)&&courseClassList.equals(other.courseClassList)&&studentClassList.equals(other.studentClassList);
+
+    }
+
+    /**
+     * returns a string representation of the class.
+     * @return a string representation of the class in format. "className, courseClassList, studentClassList"
+     */
+    public String toString()
+    {
+        return className + courseClassList.toString() + studentClassList.toString();
+    }
+
+
 
 }
--- src/Model/ClassList.java
+++ src/Model/ClassList.java
@@ -1,4 +1,57 @@
 package Model;
+import java.util.ArrayList;
 
+
+/**
+ * A class containing  list of class objects
+ * @author Sid
+ * @version 1.0
+ */
 public class ClassList {
+    private ArrayList<Class> classes;
+
+    /**
+     * No-argument constructor initializing class list.
+     */
+
+    public ClassList()
+    {
+        classes = new ArrayList<Class>();
+
+    }
+
+    /**
+     * adds a class to the class list.
+     * @param classObject a class that needs to be added to the class list.
+     */
+    public void addClass(Class classObject)
+    {
+        classes.add(classObject);
+    }
+
+    /**
+     * removes a class from the class list
+     * @param classObject a class that needs to be removed from the class list
+     */
+    public void removeClass(Class classObject)
+    {
+        classes.remove(classObject);
+
+    }
+
+    /**
+     * a method that returns a string representation of the class list.
+     * @return a string representation of the class list in format: "Class"
+     */
+    public String toString()
+    {
+        String returnString ="";
+        for (int i=0; i<classes.size(); i++)
+        {
+            returnString += classes.get(i);
+        }
+        return returnString;
+    }
+
+
 }
