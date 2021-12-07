8a269b0db47964c66c187a6d00c307fb9a7fb14d
Sid <gr.siddartha@googlemail.com>
2021-12-07 19:14:58 +0100

    Made comments
    I made some comments

--- src/Model/Lesson.java
+++ src/Model/Lesson.java
@@ -1,5 +1,11 @@
 package Model;
 
+/**
+ * A class containing Lesson information
+ * @author Sid and Bhupas
+ * @version 1.0
+ */
+
 public class Lesson {
     private TimeSlot timeSlot;
     private Course course;
@@ -10,6 +16,12 @@ public class Lesson {
     private RoomList rooms;
     private CourseList courses;
 
+    /**
+     * 3 argument constructor initializing lesson.
+     * @param timeSlot the time in which the lesson takes place
+     * @param course the course that will be taught
+     * @param room the room the lesson will take place in
+     */
     public Lesson(TimeSlot timeSlot, Course course, Room room)
     {
         this.timeSlot=timeSlot;
@@ -18,23 +30,127 @@ public class Lesson {
         this.room1=null;
         this.room2=null;
     }
+
+    /**
+     * sets the course that will be taught in the lesson
+     * @param course the course that will be taught
+     */
     public void setCourse(Course course)
     {
         this.course=course;
     }
+
+    /**
+     * returns the course that will be taught in the lesson
+     * @return the course that will be taught in the lesson
+     */
+    public Course getCourse()
+    {
+        return course;
+    }
+
+    /**
+     * sets the time the lesson will take place in
+     * @param timeSlot the time in which the lesson will take place in
+     */
     public void setTimeSlot(TimeSlot timeSlot)
     {
         this.timeSlot=timeSlot;
     }
+
+    /**
+     * returns the time the lesosn will take place in
+     * @return the time in which the lesson will take place in
+     */
+    public TimeSlot getTimeSlot()
+    {
+        return timeSlot;
+    }
+
+    /**
+     * books the room the lesson will take place in
+     * @param room the room the lesson will take place in
+     */
     public void bookRoom(Room room)
     {
         this.room=room;
     }
+
+    /**
+     * returns the room the lesson will take place in
+     * @return the room the lesson will take place in
+     */
+    public Room getRoom()
+    {
+        return room;
+    }
+
+    /**
+     * books the rooms the lesson will take place in if multiple rooms are chosen, if they are double rooms
+     * @param room1 the first of the double room
+     * @param room2 the second of the double room
+     */
     public void bookRooms(Room room1, Room room2)
     {
-        this.room1=room1;
-        this.room2=room2;
-        this.room=null;
+        if (room1.getRoomNumber().charAt(1)==room2.getRoomNumber().charAt(1) && room1.getRoomNumber().charAt(0)==room2.getRoomNumber().charAt(0) && room1.getRoomNumber().charAt(2)!=room2.getRoomNumber().charAt(2)){
+            this.room1 = room1;
+            this.room2 = room2;
+            this.room = null;
+        }
+    }
+
+    /**
+     * returns the double room the lesson will take place in
+     * @return the double room the lesson will take place in
+     */
+    public RoomList getRooms()
+    {
+        RoomList rooms = new RoomList();
+        rooms.addRoom(room1);
+        rooms.addRoom(room2);
+        return rooms;
+    }
+
+    /**
+     * compares two lessons and checks if they take place in a similar time and space
+     * @param lesson2 the second lesson that needs to be added
+     * @return true if the lessons take place in a similar space and time and are thus incopatible
+     */
+    public boolean isUnavailable(Lesson lesson2)
+    {
+        return this.room.equals(lesson2.room) && ((this.timeSlot.equals(lesson2.timeSlot) || (this.timeSlot.overLap(lesson2.timeSlot))));
+
+    }
+
+    /**
+     * compares timeslot, course, room or rooms of two lessons
+     * @param obj the lesson to compare with
+     * @return true if the given object is equal to this lesson
+     */
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Lesson))
+        {
+            return false;
+        }
+        Lesson other = (Lesson) obj;
+        return this.timeSlot.equals(other.timeSlot) && this.course.equals(other.course) && this.room.equals(other.room) && this.room1.equals(other.room1) && this.room2.equals(other.room2);
+    }
+
+    /**
+     * returns a string representation of the lesson
+     * @return a string representation of the lesson in format "timeslot, course, room/room1+room2"
+     */
+    public String toString()
+    {
+        if (room==null)
+        {
+            return timeSlot+""+course+room1+room2;
+        }
+        else
+        {
+            return timeSlot+""+course+room;
+        }
     }
 
 
--- src/Model/TimeSlot.java
+++ src/Model/TimeSlot.java
@@ -1,12 +1,22 @@
 package Model;
 
+/**
+ * A class for the timeslot object
+ * @author Sid and Bhupas
+ * @version 1.0
+ */
 public class TimeSlot {
 
     private int time;
     private int length;
     private char day;
 
-
+    /**
+     * 3 argument constructor initializing the timeslot
+     * @param time the starting time in format 0000
+     * @param length the length of the time in minutes
+     * @param day indicates the day of the week using 0, 1, 2, 3, 4, 5, 6 (0 is sunday)
+     */
     public TimeSlot(int time, int length, char day)
     {
         this.time=time;
@@ -14,14 +24,30 @@ public class TimeSlot {
         this.day=day;
 
     }
+
+    /**
+     * returns the starting time in format 0000
+     * @return the starting time in format 0000
+     */
     public int getStart()
     {
         return time;
     }
+
+    /**
+     * returns the ending time in format 0000
+     * @return the starting time plus the length
+     */
     public int getEnd()
     {
         return time+length;
     }
+
+    /**
+     * compares two different timeslots and checks if they overlap or not
+     * @param newTimeSlot a new timeslot to ompare the other one to
+     * @return true or false, depending on if it overlaps or not
+     */
     public boolean overLap(TimeSlot newTimeSlot)
     {
         if (this.day!= newTimeSlot.day)
