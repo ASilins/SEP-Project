fbaa193fc8db09500a09f52be67986e0dc258546
Ondrej <klimekondrej01@gmail.com>
2021-12-12 17:57:57 +0100

    Attempted to make students work
    I failed

--- src/View/StudentsController.java
+++ src/View/StudentsController.java
@@ -1,5 +1,6 @@
 package View;
 
+import Model.StudentList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,26 +10,40 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class StudentsController
 {
-  @FXML
-  Button btnHome, btnSave;
-  Hyperlink websiteLink;
+  @FXML private Button btnHome, btnSave;
+  @FXML private Hyperlink websiteLink;
+  @FXML private ListView allStudentsList;
+  private StudentList students;
 
   public void switchToSceneHome(ActionEvent event) throws IOException
   {
     Parent root = FXMLLoader.load(
-        Objects.requireNonNull(getClass().getResource("Home.fxml"))
-    );
-    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Objects.requireNonNull(getClass().getResource("Home.fxml")));
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     Stage window = (Stage) btnHome.getScene().getWindow();
     window.setScene(new Scene(root, 645, 720));
 
     window.show();
 
   }
+
+  public void initialize(URL url, ResourceBundle rb)
+  {
+    for (int i = 0; i < students.size(); i++)
+    {
+      allStudentsList.getItems().add(students.get(i));
+    }
+    allStudentsList.getItems().addAll("Hello", "Hi", "Bye");
+  }
+
 }
